4538b2d8e332cf0b561330e6cb94582a57236377
shimakaze-git <shimakaze.soft+github@googlemail.com>
2019-09-14 21:48:16 +0900

    feat: add usecase code.

--- ddd-django/polls_app/polls/factories.py
+++ ddd-django/polls_app/polls/factories.py
@@ -1,3 +1,5 @@
+from .use_cases import ShowVoteResultsUsecase
 
-def build_show_vote_results_use_case():
-    return None
+
+def build_show_vote_results_use_case() -> ShowVoteResultsUsecase:
+    return ShowVoteResultsUsecase()
--- ddd-django/polls_app/polls/use_cases.py
+++ ddd-django/polls_app/polls/use_cases.py
@@ -1,10 +1,41 @@
+from django.db.models import Avg, Max, Min
+from .models import Choice, Question
+
+
 class ShowVoteResultsUsecase:
 
-    def __init__(self):
-        pass
+    # def __init__(self):
+    #     pass
 
-    def execute(self):
-        return None
+    def execute(self, question_id: int) -> dict:
+        # 投票内容を取り出す
+        question = Question.objects.filter(pk=question_id).first()
+
+        # 集計する
+        aggregates = self.aggregate()
+
+        # 整形処理
+        max = aggregates[0]
+        min = aggregates[1]
+        avg = aggregates[2]
+
+        results = {
+            'question': question,
+            'max': max,
+            'min': min,
+            'avg': avg
+        }
+
+        return results
+
+    def aggregate(self) -> tuple:
+        """集計する."""
+
+        max = Choice.objects.aggregate(Max('votes'))['votes__max']
+        min = Choice.objects.aggregate(Min('votes'))['votes__min']
+        avg = Choice.objects.aggregate(Avg('votes'))['votes__avg']
+
+        return max, min, avg
 
 
 class CreateUserUsecase:
